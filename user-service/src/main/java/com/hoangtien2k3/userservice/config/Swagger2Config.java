0f4c5e29da3fdfb4a8edab62d723662650eda4bf
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2025-10-20 23:40:10 +0530

    Update Swagger2Config.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/config/Swagger2Config.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/config/Swagger2Config.java
@@ -1,23 +1,16 @@
 package com.hoangtien2k3.userservice.config;
 
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
-import org.springframework.boot.actuate.endpoint.web.*;
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.util.StringUtils;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Configuration
@@ -28,22 +21,22 @@ public class Swagger2Config {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.hoangtien2k3.userservice"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiEndPointsInfo())
+                .apiInfo(apiInfo())
                 .securitySchemes(List.of(apiKey()))
                 .securityContexts(List.of(securityContext()));
     }
 
-    private ApiInfo apiEndPointsInfo() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("USER-SERVICE API DOCUMENTATIONS")
-                .description("API Documentation's USER-SERVICE")
+                .title("USER-SERVICE API")
+                .description("API Documentation for User Service")
                 .contact(new Contact("hoangtien2k3", "https://hoangtien2k3qx1.github.io/", "hoangtien2k3qx1@gmail.com"))
+                .version("1.0.0")
                 .license("MIT")
                 .licenseUrl("https://mit-license.org/")
-                .version("1.0.0")
                 .build();
     }
 
@@ -61,39 +54,4 @@ public class Swagger2Config {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         return List.of(new SecurityReference("JWT", new AuthorizationScope[]{authorizationScope}));
     }
-
-    /**
-     * Fixed WebMvcEndpointHandlerMapping for Spring Boot 3
-     */
-    @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(
-            WebEndpointsSupplier webEndpointsSupplier,
-            ServletEndpointsSupplier servletEndpointsSupplier,
-            ControllerEndpointsSupplier controllerEndpointsSupplier,
-            EndpointMediaTypes endpointMediaTypes,
-            CorsEndpointProperties corsProperties,
-            WebEndpointProperties webEndpointProperties,
-            Environment environment) {
-
-        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-        allEndpoints.addAll(webEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-
-        String basePath = webEndpointProperties.getBasePath();
-        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-        boolean shouldRegisterLinksMapping = webEndpointProperties.getDiscovery().isEnabled() &&
-                (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-
-        // Fixed: removed extra null argument
-        return new WebMvcEndpointHandlerMapping(
-                endpointMapping,
-                allEndpoints,
-                endpointMediaTypes,
-                corsProperties.toCorsConfiguration(),
-                new EndpointLinksResolver(allEndpoints, basePath),
-                shouldRegisterLinksMapping
-        );
-    }
 }
-
